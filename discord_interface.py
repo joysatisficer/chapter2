56f85bd0bc8fa172f03ff80a7a6951da9b00f918
ampdot <ampdot@mesh.host>
2023-04-05 19:34:21 -0400

    Gracious exit, move discord things to another file, and schedule instead of awaiting typing

--- discord_interface.py
+++ discord_interface.py
@@ -1,27 +1,34 @@
 import asyncio
-import re
+import contextlib
+import signal
 import time
-from typing import AsyncIterable, TypeVar, Callable, Awaitable, List
+from typing import AsyncIterable, TypeVar, Callable, Awaitable
 
-from declarations import GenerateResponse, Message, UserID, Author, MessageHistory, Action
+from declarations import GenerateResponse, Message, UserID, Author
 
 import discord
-import discord.utils
+
+from discord_improved import ScheduleTyping, parse_discord_content
 
 
 class DiscordInterface(discord.Client):
+    MAX_CONCURRENT_MESSAGES = 100_000
+
     def __init__(self, generate_response: GenerateResponse) -> None:
         intents = discord.Intents.default()
         intents.message_content = True
         super().__init__(intents=intents)
         self.generate_response: GenerateResponse = generate_response
+        self.message_semaphore = asyncio.BoundedSemaphore(self.MAX_CONCURRENT_MESSAGES)
+        self.pending_shutdown = False
+        signal.signal(signal.SIGINT, self.shutdown)
 
     async def on_message(self, message: discord.Message) -> None:
         # check if we should respond to the message
         if not await self.should_reply(message):
             return
-        async with message.channel.typing():
-            my_user_id = await discord_user_to_user_id(self.user)
+        async with self.handle_exceptions(message):
+            my_user_id = discord_user_to_user_id(self.user)
             response_messages = self.generate_response(
                 my_user_id,
                 map_async_iterator(
@@ -29,15 +36,16 @@ class DiscordInterface(discord.Client):
                     self.discord_message_to_message
                 )
             )
-            async for reply_message in response_messages:
-                if reply_message.author.user_id == my_user_id:
-                    await wait_until_timestamp(reply_message.timestamp, message.channel.typing)
-                    await message.channel.send(reply_message.message)
+            async with ScheduleTyping(message.channel):
+                async for reply_message in response_messages:
+                    if reply_message.author.user_id == my_user_id:
+                        await wait_until_timestamp(reply_message.timestamp, message.channel.typing)
+                        await message.channel.send(reply_message.message)
 
     async def discord_message_to_message(self, message: discord.Message) -> Message:
         return Message(
             Author(
-                await discord_user_to_user_id(message.author),
+                discord_user_to_user_id(message.author),
                 message.author.name
             ),
             await parse_discord_content(message),
@@ -46,6 +54,23 @@ class DiscordInterface(discord.Client):
 
     async def should_reply(self, message: discord.Message):
         return message.author != self.user
+
+    def shutdown(self, sig, frame):
+        self.pending_shutdown = True
+        if self.message_semaphore._value == self.MAX_CONCURRENT_MESSAGES:
+            raise KeyboardInterrupt()
+
+    @contextlib.asynccontextmanager
+    async def handle_exceptions(self, message: discord.Message):
+        try:
+            async with self.message_semaphore:
+                yield None
+        except Exception as e:
+            await message.add_reaction('⚠')
+            raise
+        finally:
+            if self.pending_shutdown and self.message_semaphore._value == self.MAX_CONCURRENT_MESSAGES:
+                await self.close()
 
 
 async def wait_until_timestamp(timestamp, coroutine):
@@ -56,7 +81,7 @@ async def wait_until_timestamp(timestamp, coroutine):
             await asyncio.sleep(timestamp - current_time)
 
 
-async def discord_user_to_user_id(user: discord.User) -> UserID:
+def discord_user_to_user_id(user: discord.User) -> UserID:
     return UserID(user.id, 'discord')
 
 
@@ -73,49 +98,3 @@ async def map_async_iterator(async_iterator: AsyncIterable[T], fn: Callable[[T],
     """
     async for item in async_iterator:
         yield await fn(item)
-
-
-async def parse_discord_content(self: discord.Message) -> str:
-    """discord.Message.clean_content() where "name" is used in place of display_name"""
-    if self.guild:
-
-        def resolve_member(id: int) -> str:
-            m = self.guild.get_member(id) or utils.get(self.mentions, id=id)  # type: ignore
-            return f'@{m.name}' if m else '@deleted-user'
-
-        def resolve_role(id: int) -> str:
-            r = self.guild.get_role(id) or utils.get(self.role_mentions, id=id)  # type: ignore
-            return f'@{r.name}' if r else '@deleted-role'
-
-        def resolve_channel(id: int) -> str:
-            c = self.guild._resolve_channel(id)  # type: ignore
-            return f'#{c.name}' if c else '#deleted-channel'
-
-    else:
-
-        def resolve_member(id: int) -> str:
-            m = discord.utils.get(self.mentions, id=id)
-            return f'@{m.name}' if m else '@deleted-user'
-
-        def resolve_role(id: int) -> str:
-            return '@deleted-role'
-
-        def resolve_channel(id: int) -> str:
-            return '#deleted-channel'
-
-    transforms = {
-        '@': resolve_member,
-        '@!': resolve_member,
-        '#': resolve_channel,
-        '@&': resolve_role,
-    }
-
-    def repl(match: re.Match) -> str:
-        type = match[1]
-        id = int(match[2])
-        transformed = transforms[type](id)
-        return transformed
-
-    result = re.sub(r'<(@[!&]?|#)([0-9]{15,20})>', repl, self.content)
-
-    return discord.utils.escape_mentions(result)
---
+++ doc/development.md
@@ -0,0 +1,16 @@
+# Development
+
+## Building
+
+```bash
+pipenv install
+```
+
+## Running tests
+
+## Running type checking
+
+## Toolchain setup
+
+When receiving SIGINT, the process will wait until zero messages are being handled before exiting. Set
+`kill.windows.processes.softly` inside IntelliJ Registry to have the "STOP" button do this.
