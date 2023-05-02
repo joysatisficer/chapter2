ecef351e9360de9a943169bd95868b489c8c1dc0
ampdot <ampdot@mesh.host>
2023-05-02 15:24:48 -0400

    Add mufflers

---
+++ src/message_formats.py
@@ -0,0 +1,24 @@
+import re
+from dataclasses import dataclass
+from functools import reduce
+from typing import Callable, Optional, List, Tuple
+
+
+@dataclass
+class MessageFormat:
+    """In the future, this could format all actions, not just messages, such as inner monologues"""
+
+    wrap: Callable[[str, str], str]
+    name_prefix: Callable[[str], str]
+    parse: Callable[[str], List[Tuple[str, str]]]
+
+
+irc_message_format = MessageFormat(
+    wrap=lambda name, message: reduce(
+        lambda acc, line: acc + "\n" + line,
+        [f"<{name}> {message}" for line in message.splitlines() if not line.isspace()],
+        "",  # initial value
+    ),
+    name_prefix=lambda name: f"<{name}>",
+    parse=lambda line: re.findall(r"(?:<([^\n]+)>)?(.+)$", line, re.MULTILINE),
+)
