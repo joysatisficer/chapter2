b051df802eedbf5ff8037e89db3b6f8138ae9cf4
CrazyPython <jamtlu@gmail.com>
2023-12-16 18:08:29 -0500

    Add syncit

---
+++ src/util/asyncit.py
@@ -0,0 +1,9 @@
+from typing import Iterable, AsyncIterable
+
+
+def eager_iterable_to_async_iterable(iterable: Iterable) -> AsyncIterable:
+    class AsyncIterableWrapper:
+        async def __aiter__(self):
+            for item in iterable:
+                yield item
+    return AsyncIterableWrapper()
