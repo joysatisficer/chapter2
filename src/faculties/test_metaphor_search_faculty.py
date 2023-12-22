8b95d23a28dbaa44f8d0b164d0e89ad51177651d
CrazyPython <jamtlu@gmail.com>
2023-12-21 23:32:35 -0500

    Improve metaphor performance

---
+++ src/faculties/test_metaphor_search_faculty.py
@@ -0,0 +1,45 @@
+import os
+from pathlib import Path
+
+import pytest
+import yaml
+
+from declarations import Message, Author
+from resolve_config import MetaphorSearchFacultyConfig, Config, load_config_from_kv
+from faculties.metaphor_search_faculty import metaphor_search_faculty
+
+
+@pytest.mark.asyncio
+async def test_metaphor_search_faculty():
+    kv = {
+        "em_folder": "/tmp",
+        "name": "tmp",
+    }
+    parent_dir = Path(__file__).resolve().parents[1]
+    try:
+        with open(os.path.expanduser("~/.config/chapter2/vendors.yaml")) as file:
+            kv = {**kv, **yaml.safe_load(file)}
+    except FileNotFoundError:
+        pass
+    try:
+        with open(parent_dir / "ems/vendors.yaml") as file:
+            kv = {**kv, **yaml.safe_load(file)}
+    except FileNotFoundError:
+        pass
+    config = load_config_from_kv(kv)
+    async for message in metaphor_search_faculty(
+        mock_message_history_iterator(config), MetaphorSearchFacultyConfig(), config
+    ):
+        print(message)
+
+
+async def mock_message_history_iterator(config: Config):
+    # todo: iterable to lazy iterable function
+    messages = [
+        Message(Author("alice"), "hello"),
+        Message(Author("bob"), "hi alice!"),
+        Message(Author(config.name), "hi bob!"),
+        Message(Author("alice"), f"hi {config.name}!"),
+    ][::-1]
+    for message in messages:
+        yield message
