76f91794dbb85561cf9dde01a403b33d59505491
ampdot <ampdot@mesh.host>
2023-12-21 19:08:35 -0500

    Reformat code

--- src/util/asyncit.py
+++ src/util/asyncit.py
@@ -6,4 +6,5 @@ def eager_iterable_to_async_iterable(iterable: Iterable) -> AsyncIterable:
         async def __aiter__(self):
             for item in iterable:
                 yield item
+
     return AsyncIterableWrapper()
