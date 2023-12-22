9fb0c9d4f6fc14af1fb21c40f9489ffe44eea427
ampdot <ampdot@mesh.host>
2023-12-21 23:32:35 -0500

    Improve metaphor performance

--- src/faculties/metaphor_search_faculty.py
+++ src/faculties/metaphor_search_faculty.py
@@ -5,12 +5,15 @@ from asgiref.sync import sync_to_async
 from aioitertools.more_itertools import take as async_take
 
 import dateutil.parser
-from metaphor_python import Metaphor
+from functools import cache
+from metaphor_python import AsyncMetaphor
 from intermodel import callgpt
 
 from declarations import ActionHistory, Message, Author
 from message_formats import MessageFormat, irc_message_format
 from resolve_config import Config, MetaphorSearchFacultyConfig
+
+SharedAsyncMetaphor = cache(AsyncMetaphor)
 
 
 async def metaphor_search_faculty(
@@ -21,13 +24,13 @@ async def metaphor_search_faculty(
         await async_take(faculty_config.recent_message_attention, history)
         + [Message(Author(config.name), "")],
     )
-    metaphor_client = Metaphor(config.metaphor_search_api_key)
+    metaphor_client = SharedAsyncMetaphor(config.metaphor_search_api_key)
     assert (
         faculty_config.max_tokens <= 11_000
     )  # max number of tokens it can return ais 10 documents * 1000 tokens per document by default
     results = sorted(
         (
-            await sync_to_async(metaphor_client.search)(
+            await metaphor_client.search(
                 trim_tokens("gpt2", message_history_string, 1000),
                 num_results=10,
                 use_autoprompt=False,
@@ -43,7 +46,7 @@ async def metaphor_search_faculty(
     )
     if len(results) == 0:
         return
-    document_contents_response = await sync_to_async(metaphor_client.get_contents)(
+    document_contents_response = await metaphor_client.get_contents(
         [result.id for result in results]
     )
     document_contents = document_contents_response.contents
