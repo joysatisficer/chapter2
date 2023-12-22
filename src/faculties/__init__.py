4b68d75992c2d6aea49a30d7068ea3616a8f5a36
ampdot <ampdot@mesh.host>
2023-12-22 06:52:52 -0500

    Move FACULTY_NAME_TO_FUNCTION to faculties

---
+++ src/faculties/__init__.py
@@ -0,0 +1,7 @@
+from faculties.character_faculty import character_faculty
+from faculties.metaphor_search_faculty import metaphor_search_faculty
+
+FACULTY_NAME_TO_FUNCTION = {
+    "character": character_faculty,
+    "metaphor_search": metaphor_search_faculty,
+}
