24e0a3aec7e5a94d51209a5c88c285538c63d034
CrazyPython <jamtlu@gmail.com>
2024-02-09 01:33:08 -0800

    Overhauled Exa integration
    * Highlights support
    * Up to 25 results per search, lazily fetched
    * "autoprompt" support
    * Support input_format

--- src/faculties/__init__.py
+++ src/faculties/__init__.py
@@ -1,7 +1,9 @@
 from faculties.character_faculty import character_faculty
-from faculties.metaphor_search_faculty import metaphor_search_faculty
+from faculties.exa_search_faculty import exa_search_faculty
 
 FACULTY_NAME_TO_FUNCTION = {
     "character": character_faculty,
-    "metaphor_search": metaphor_search_faculty,
+    "exa_search": exa_search_faculty,
+    # deprecated names
+    "metaphor_search": exa_search_faculty,
 }
