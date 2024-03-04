781f4d83880313ada36b6d4575d54778bbc21642
ampdot <ampdot@mesh.host>
2024-03-04 06:54:00 -0800

    Fix (?) test

--- chapter2/test_retriever.py
+++ chapter2/test_retriever.py
@@ -5,7 +5,7 @@ class TestSVMIndex:
     def setup_method(self):
         self.index = SVMIndex("intfloat/e5-large-v2")
 
-    def test_retrieval(self):
-        self.index.add_data(["hello", "world"])
-        strings = self.index.query("hi", 1)
-        assert strings[0] == "hello"
+    async def test_retrieval(self):
+        await self.index.add_data(["hello", "world"], ["hellokey", ["worldkey"]])
+        strings = await self.index.query("hi", 1)
+        assert strings[0] == "hellokey"
