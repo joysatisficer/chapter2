4b075e1d3ff0fe5eff3ff40bd4924697ac82d34f
ampdot <ampdot@mesh.host>
2024-10-16 16:39:18 +0900

    Trace airtable notes

--- chapter2/faculties/contrib/airtable_notes_faculty.py
+++ chapter2/faculties/contrib/airtable_notes_faculty.py
@@ -1,8 +1,10 @@
 from declarations import ActionHistory, Message, Author
 from ontology import AirtableNotesFacultyConfig, EmConfig
 from asgiref.sync import sync_to_async
+from trace import trace
 
 
+@trace
 async def airtable_notes_faculty(
     history: ActionHistory, faculty_config: AirtableNotesFacultyConfig, em: EmConfig
 ):
